ae5e9c4a332ef3dbddfc93e08a0c6e31ea122a3e
maggielumaggie <mqlu@outlook.com>
2023-04-06 19:34:07 -0700

    a9

---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const schema = mongoose.Schema({
+  tuit: String,
+  likes: Number,
+  liked: Boolean,
+  topic: String,
+  time: String,
+  title: String,
+  image: String,
+  userName: String,
+  handle: String,
+  replies: Number,
+  retuits: Number,
+  dislikes: Number,
+}, {collection: 'tuits'});
+
+export default schema;
